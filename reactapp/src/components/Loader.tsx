343cff229b4405d6497ce8d3d95eb02f220a8a8f
cxstackwisr <ehiremengold@gmail.com>
2025-04-07 17:37:19 +0100

    refactor: improve code consistency and add loading state for server wake-up

---
+++ reactapp/src/components/Loader.tsx
@@ -0,0 +1,24 @@
+import "./loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loading-container">
+      <svg viewBox="0 0 900 300">
+        <text
+          x="50%"
+          y="50%"
+          dy=".32rem"
+          textAnchor="middle"
+          className="text-body"
+        >
+          SBSC
+        </text>
+      </svg>
+      <p className="text-center text-white font-bold text-sm lg:text-xl">
+        waking up server...
+      </p>
+    </div>
+  );
+};
+
+export default Loader;
